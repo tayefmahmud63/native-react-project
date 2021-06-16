b5de1b256ba807e3cee193ce2c54038417b4c0ea
YajanaRao <yajananrao@gmail.com>
2021-06-16 12:20:11 +0530

    updated progressbar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,37 +1,20 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { ProgressBar, Text } from 'react-native-paper';
+import * as React from 'react';
+import { View } from "react-native";
+import { ProgressBar } from "react-native-paper";
 import TrackPlayer from 'react-native-track-player';
 export class Progress extends TrackPlayer.ProgressComponent {
 
   render() {
     return (
       // Note: formatTime and ProgressBar are just examples:
-      <View style={styles.view}>
-        <Text>{this.state.position}</Text>
-
+      <View style={{ flex: 1, width: "100%" }}>
         <ProgressBar
           progress={this.getProgress()}
-          buffered={this.getBufferedProgress()}
+        // style={{ height: 6 }}
+        // buffered={this.getBufferedProgress()}
         />
       </View>
     );
   }
 
 }
-
-
-const styles = StyleSheet.create({
-  view: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    height: 40,
-    width: '100%',
-  },
-  bar: {
-    height: 30,
-    width: '100%',
-  },
-});
