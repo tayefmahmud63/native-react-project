0c865a97f38fb4b034b0836f9955b986c7139a3d
YajanaRao <yajananrao@gmail.com>
2022-07-22 22:10:19 +0530

    Removed google sign in

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { log } from './logging';
 
 const fetchLyrics = (artistName: string, songName: string) => {
@@ -27,27 +26,5 @@ const getLyrics = async song => {
   }
 };
 
-async function validateAccessToken(token: string) {
-  const validationUrl = `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`;
-  const response = await fetch(validationUrl);
-  const message = await response.json();
-  if ('error' in message) {
-    log.error('validateAccessToken', message);
-    return false;
-  }
-  return true;
-}
-
-export async function getAccessToken() {
-  const token = await AsyncStorage.getItem('@token');
-  const isValid = await validateAccessToken(token);
-  log.debug('getAccessToken', `Is token valid: ${isValid}`);
-  if (!isValid) {
-    const { accessToken } = await GoogleSignin.getTokens();
-    AsyncStorage.setItem('@token', accessToken);
-    return accessToken;
-  }
-  return token;
-}
 
 export default getLyrics;
